42c06ee9e1b7d66a01551543ec6d0b08047b0cc9
Balbina <balbinasantana@gmail.com>
2016-02-21 04:58:32 -0600

    plain old front end

--- app/helpers/logout_helper.rb
+++ app/helpers/logout_helper.rb
@@ -1,9 +1,9 @@
 module LogoutHelper
   def logout_link
     if current_hacker
-      link_to('Logout', destroy_hacker_session_path, method: :delete, class: 'btn btn-danger')
+      link_to('Logout', destroy_hacker_session_path, method: :delete, class: 'btn')
     elsif current_student
-      link_to('Logout', destroy_student_session_path, method: :delete, class: 'btn btn-danger')
+      link_to('Logout', destroy_student_session_path, method: :delete, class: 'btn')
     else
       ''
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :hackers
 
   get 'hacker/profile' => 'hackers#show', as: :hacker
+  get 'student/profile' => 'students#show', as: :student
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
