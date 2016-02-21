db89fe09dc5dbc1e266f5f7171016328b39940b6
Balbina <balbinasantana@gmail.com>
2016-02-21 10:36:59 -0600

    student profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def current_user
+    @current_user ||= current_hacker || current_student
+  end
 
   protected
 
