ffc2626adb7aa7a0ca347d607213d06f3fd4b957
Andres Lopez De Leon <agldeleon@gmail.com>
2016-02-21 09:46:44 -0600

    Allow for information changes to hacker profile

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -2,4 +2,19 @@ class HackersController < ApplicationController
   before_action :authenticate_hacker!
   def show
   end
+
+  def update
+	  if current_hacker.update(hacker_params)
+	    render :show, notice: 'Information was successfully updated.'
+	  else
+	    render :show, error: 'Information was not successfully updated.'
+	  end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def hacker_params
+      params.require(:hacker).permit(:language_ids)
+    end
 end
